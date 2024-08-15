b3bbc9e5b248f75a35ea279fb4778c6b1c2b5ced
wbdc_truong.tran <truong.tran@wtc.woowahan.com>
2024-08-15 14:55:27 +0700

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/hWXQZ4kXNg

---
+++ apps/admin-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@libs/components';
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
